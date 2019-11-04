bff2c03cbcfbdb58b9013ad67119f78b99e39c8d
resdust <resdust_li@163.com>
2019-11-04 16:37:33 +0800

    modified pre module

---
+++ code/pre/pre_features.py
@@ -0,0 +1,90 @@
+import re
+
+# decode URL, HTML, HEX, Unicode
+def decode(data):
+    import urllib
+    import html
+
+    data = data.rstrip()
+    data = urllib.parse.unquote(data)
+    data = html.unescape(data)
+    return data
+
+def get_len(url):
+    return len(url)
+
+def get_url_count(url):
+    if re.search('(http)|(https)', url, re.IGNORECASE) :
+        return 1
+    else:
+        return 0
+
+def get_evil_char(url):
+    # return len(re.findall("[`~!@#$%^*(_)-+=<>:\"{}| ,./;\'\\[]·]", url, re.IGNORECASE))
+    return len(re.findall("[<>,\'\"/]", url,re.IGNORECASE))
+
+def get_evil_word(url):
+    XSS = ["window","location","this","iframe","response_write","prompt","javascript",
+    "document","cookie","alert","script","onerror","onload","eval","createelement",
+    "string","search","cmd","div","img","<script","href","src","var","prompt",".js"]
+    # XSS = ["response_write","prompt","javascript",
+    # "cookie","alert","script","onerror","onload","eval","createelement",
+    # "cmd","<script","href","src",".js"]
+    XSS = '(' + ')|('.join(XSS) + ')'
+    return len(re.findall( XSS, url, re.IGNORECASE))
+
+def get_feature(url):
+    return [get_len(url),get_evil_char(url),get_evil_word(url)]
+
+def do_metrics(y_test,y_pred):
+    from sklearn import metrics
+    
+    # [[TP, FN], [FP, TN]]
+    print ("metrics.confusion_matrix:  [[TP, FN], [FP, TN]] \n", metrics.confusion_matrix(y_test, y_pred))
+    # TP+TN/(TP+TN+FP+NP)
+    print ("metrics.accuracy_score:", metrics.accuracy_score(y_test, y_pred))
+    # TP/(TP+FP)
+    print ("metrics.precision_score:", metrics.precision_score(y_test, y_pred))
+    # TP/(TP+FN)
+    print ("metrics.recall_score:", metrics.recall_score(y_test, y_pred))
+    print ("metrics.f1_score:", metrics.f1_score(y_test,y_pred))
+
+# extract features to list and dump into file
+# return labels
+def extract_feature(filename,data,y,isxss):
+    import pickle
+
+    pk_file = filename[:-3]+'pickle'
+    start = time.time() 
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            line = decode(line)
+            f1=get_len(line)
+            f2=get_url_count(line)
+            f3=get_evil_char(line)
+            f4=get_evil_word(line)
+            data.append([f1,f2,f3,f4])
+            y.append(isxss)
+    end = time.time()
+    print('Finish feature extraction in %.4f.' %(end-start))
+
+    with open(pk_file, 'wb') as pkf:
+        pickle.dump(data, pkf)
+        
+    return data
+
+def normalization(x):
+    mean = x.mean(axis=0)
+    std = x.std(axis=0)
+    x -= mean
+    x /= std
+
+if __name__ == "__main__":
+    import time 
+
+    x = [] # feature data vectors
+    y = [] # labels
+    
+    extract_feature('data/good_example.csv',x,y,0)
+    extract_feature('data/xss_example.csv',x,y,1)
+    
--- code/pre/pre_text.py
+++ code/pre/pre_text.py
@@ -43,12 +43,12 @@ def dictionary_set(data):
     words = [] # [token1, token2...]
     for line in data:
         words += line
-    # build a most common dictionary
+    # build a dictionary with most common words
     count = [['WORD', -1]]
     counter = Counter(words)
     count.extend(counter.most_common(dictionary_size-1))
     dictionary = [word if num>1 else 0 for word,num in count]
-    # transfer segment data to words in dictionary or 'WORD'
+    # transfer segment data to word in dictionary or 'WORD'
     datas = []
     for line in data:
         for i in range(len(line)):
@@ -66,32 +66,39 @@ def word2vec(datas):
     embedding_size=256
     skip_window=5
     num_sampled=64
-    # word vector length:250
     num_iter=5
     model=Word2Vec(datas,size=embedding_size,
         window=skip_window,negative=num_sampled,iter=num_iter)
     embeddings=model.wv
     return embeddings    
 
-# extract scripts from File to List
-def f2l(filename,data,isxss):
-    pk_file = filename[:-3]+'pickle'
+# extract word embeddings and dump into file
+# return labels
+def word_embed(filename,data,isxss):
+    import time
+
+    pk_file = filename[:-4]+'_text.pickle'
+    start = time.time()
     with open(filename, encoding="utf-8") as f:
         for line in f:
             line = decode(line)
             token = GeneSeg(line)
-            token.append(str(isxss))
             data.append(token)
+            y.append(isxss)
     datas = dictionary_set(data)
     embeddings = word2vec(datas)
-    print(embeddings)
+    end = time.time()
+    print('Finish preprocess to word embeddings in %4f.' %(end-start))
 
     with open(pk_file, 'wb') as pkf:
         pickle.dump(embeddings, pkf)
-    return datas
+    print('model.wv has been dumped into ', pk_file)
+
+    return y
 
 if __name__ == "__main__":
     x = [] # script extract from file
+    y = [] # lables
     good_example = 'xss_detector\\data\\good_sample.csv'
     xss_example = 'xss_detector\\data\\xss_sample.csv'
     f2l(good_example,x,0)
